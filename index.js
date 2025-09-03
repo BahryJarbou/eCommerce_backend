f8f6f19de8bd8f4739617cf3c5e83a34a2f9b041
Bahry <bahry.jarbou@gmail.com>
2025-09-03 16:21:30 +0200

    expanded validation to cover category and product schemas

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import "./db/associations.js";
 import express from "express";
 import userRouter from "./routers/userRouter.js";
-import validateSchema from "./middlewares/validationMiddleware.js";
-import UserSchema from "./schemas/userSchema.js";
+import categoryRouter from "./routers/categoryRouter.js";
+import productRouter from "./routers/productRouter.js";
+
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-
 app.use("/users", userRouter);
+app.use("/categories", categoryRouter);
+app.use("/products", productRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,18 +1,36 @@
-import userSchema from "../schemas/userSchema.js";
+import UserSchema from "../schemas/userSchema.js";
+import CategorySchema from "../schemas/categorySchema.js";
+import ProductSchema from "../schemas/productSchema.js";
 
 const validateSchema = (params) => (req, res, next) => {
   const result = params.safeParse(req.body);
 
-  if (params === userSchema) {
+  if (params === UserSchema) {
     if (!result.success) {
       throw new Error(
         "Name, email, and password are required and in the right form.",
         { cause: 400 }
       );
+    } else {
+      next();
     }
   }
-
-  next();
+  if (params === CategorySchema) {
+    if (!result.success) {
+      throw new Error("Name is required", { cause: 400 });
+    } else {
+      next();
+    }
+  }
+  if (params === ProductSchema) {
+    if (!result.success) {
+      throw new Error("Name, description, and price are required.", {
+        cause: 400,
+      });
+    } else {
+      next();
+    }
+  }
 };
 
 export default validateSchema;
